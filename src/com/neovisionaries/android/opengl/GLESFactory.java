5298d917fec6106800db2bb7ba047ccaa68fc758
Takahiko Kawasaki <takahiko.kawasaki@neovisionaries.com>
2012-04-19 14:10:28 +0900

    Get initializeInstance(String) to GLESFactory.

--- src/com/neovisionaries/android/opengl/GLESFactory.java
+++ src/com/neovisionaries/android/opengl/GLESFactory.java
@@ -21,9 +21,11 @@ import android.os.Build;
  * </p>
  *
  * <p>
- * The system property defined by {@link #GLES_IMPL_CLASS_PROPERTY}
- * can be used to specify a class name of an implementation of
- * {@link GLES} interface.
+ * If you want to make {@link #getInstance()} return another
+ * different {@link GLES} implementation, call {@link
+ * #initializeInstance(String)} or set a class name to the
+ * system property {@link #GLES_IMPL_CLASS_PROPERTY}. See
+ * the description of {@link #getInstance()} for details.
  * </p>
  *
  * @author Takahiko Kawasaki
@@ -47,7 +49,7 @@ public final class GLESFactory
      * The class name of the default implementation of
      * {@link GLES} interface for OpenGL ES 2.0.
      */
-    private static final String GLES_IMPL_GLES20
+    public static final String GLES_IMPL_GLES20
         = "com.neovisionaries.android.opengl.impl.GLESImpl20";
 
 
@@ -55,7 +57,7 @@ public final class GLESFactory
      * The class name of the default implementation of
      * {@link GLES} interface for OpenGL ES 1.1.
      */
-    private static final String GLES_IMPL_GLES11
+    public static final String GLES_IMPL_GLES11
         = "com.neovisionaries.android.opengl.impl.GLESImpl11";
 
 
@@ -66,12 +68,88 @@ public final class GLESFactory
 
 
     /**
+     * Initialize the singleton instance of {@link GLES} implemetation.
+     *
+     * <p>
+     * You do not have to call this method if the default implementation
+     * suits your needs. See the description of {@link #getInstance()}
+     * for the case you need to call this method.
+     * </p>
+     *
+     * @param className
+     *         The name of a class that implements {@link GLES} interface.
+     *
+     * @throws IllegalArgumentException
+     *         The given argument is null.
+     *
+     * @throws IllegalStateException
+     *         The singleton instance has aleady been created.
+     *
+     * @throws ClassNotFoundException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     *
+     * @see #getInstance()
+     */
+    public static void initializeInstance(String className)
+            throws ClassNotFoundException, InstantiationException, IllegalAccessException
+    {
+        if (className == null)
+        {
+            throw new IllegalArgumentException("className is null.");
+        }
+
+        synchronized (GLESFactory.class)
+        {
+            if (instance != null)
+            {
+                throw new IllegalStateException("Already initialized.");
+            }
+
+            instance = createInstance(className);
+        }
+    }
+
+
+    /**
      * Get an implementation of {@link GLES} interface.
      *
      * <p>
-     * The system property defined by {@link #GLES_IMPL_CLASS_PROPERTY}
-     * can be used to specify a class name of an implementation of
-     * {@link GLES} interface.
+     * If {@link #initializeInstance(String)} has been called before
+     * this method is called, the object created in the call of
+     * {@link #initializeInstance(String)} is returned. In other words,
+     * to use another implementation of {@link GLES} that is different
+     * from the one selected by the steps described below, call {@link
+     * #initializeInstance(String)} before this method. Note that
+     * because getInstance() method is called at various places in the
+     * implementation of this Java package, if you want to use {@link
+     * #initializeInstance(String)}, you must call it before not only
+     * calling getInstance() but also referring to any other class in
+     * this Java package.
+     * </p>
+     *
+     * <p>
+     * The steps to select an implementation of {@link GLES} instance
+     * are as follows.
+     * </p>
+     *
+     * <ol>
+     * <li>The system property of {@link #GLES_IMPL_CLASS_PROPERTY}
+     *     is checked. If it holds a non-null value, the value is
+     *     used as a class name of a {@link GLES} implementation.
+     * <li>Otherwise, the API Level of the Android platform is checked.
+     *     If the API Level is 8 (Android 2.2) or higher, the value of
+     *     {@link #GLES_IMPL_GLES20} is used as a class name of a
+     *     {@link GLES} implementation.
+     * <li>Otherwise, the value of {@link #GLES_IMPL_GLES11} is used
+     *     as a class name of a {@link GLES} implementation.
+     * </ol>
+     *
+     * <p>
+     * If you want to use OpenGL ES 1.1 instead of OpenGL ES 2.0 on
+     * Android 2.2 (API Level 8) or newer, you need to call {@link
+     * #initializeInstance(String)} or set a class name to the
+     * system property {@link #GLES_IMPL_CLASS_PROPERTY}.
      * </p>
      *
      * @return
@@ -83,19 +161,24 @@ public final class GLESFactory
      */
     public static GLES getInstance()
     {
-        if (instance == null)
+        synchronized (GLESFactory.class)
         {
-            try
+            if (instance == null)
             {
-                // Create an object that implements GLES interface.
-                instance = createInstance();
-            }
-            catch (Exception e)
-            {
-                // Failed to create an object implementing GLES interface.
-                String message = "Failed to create an object implementing GLES interface.";
+                String className = getImplementationClassName();
 
-                throw new UnsupportedOperationException(message, e);
+                try
+                {
+                    // Create an object that implements GLES interface.
+                    instance = createInstance(className);
+                }
+                catch (Exception e)
+                {
+                    // Failed to create an object implementing GLES interface.
+                    String message = "Failed to create an object implementing GLES interface.";
+
+                    throw new UnsupportedOperationException(message, e);
+                }
             }
         }
 
@@ -105,8 +188,9 @@ public final class GLESFactory
 
     /**
      * Create an object that implements {@link GLES} interface.
-     * The class name of the object is determined by {@link
-     * #getImplementationClassName()}.
+     *
+     * @param className
+     *         The name of a class that implements {@link GLES} interface.
      *
      * @return
      *         An object that implements {@link GLES} interface.
@@ -115,19 +199,17 @@ public final class GLESFactory
      * @throws InstantiationException
      * @throws IllegalAccessException
      */
-    private static GLES createInstance() throws ClassNotFoundException, InstantiationException, IllegalAccessException
+    private static GLES createInstance(String className)
+            throws ClassNotFoundException, InstantiationException, IllegalAccessException
     {
-        // Get a class name of an implementation of GLES interface.
-        String className = getImplementationClassName();
-
         // Load the class.
         Class<?> clazz = Class.forName(className, true, GLESFactory.class.getClassLoader());
 
         // Create an instance of the class.
         Object impl = clazz.newInstance();
 
-        // Cast the object to GLES interface. This may throw
-        // a ClassCastException.
+        // Cast the object to GLES interface.
+        // This may throw ClassCastException.
         return (GLES)impl;
     }
 
